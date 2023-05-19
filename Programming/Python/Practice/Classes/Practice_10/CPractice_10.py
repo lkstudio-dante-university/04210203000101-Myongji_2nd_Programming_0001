81c31e02f7d9eba0f6c12fef14b4d2809cbca1d9
dante <lkstudio.dante@gmail.com>
2023-05-19 09:27:26 +0900

    no message

--- Programming/Python/Practice/Classes/Practice_10/CPractice_10.py
+++ Programming/Python/Practice/Classes/Practice_10/CPractice_10.py
@@ -74,8 +74,29 @@ class CPractice_10(QMainWindow, uic.loadUiType("Resources/Practice_10/P10MainWin
 		oPainter = QPainter(self)
 		
 		try:
-			pass
-		
+			nNumPositions = len(self.m_oPosList)
+			
+			# 위치 정보가 없을 경우
+			if nNumPositions < 2:
+				return
+			
+			# 펜 일 경우
+			if self.m_eSelTool == CPractice_10.EP10Tool.PEN:
+				for i in range(0, nNumPositions - 1):
+					oPainter.drawLine(self.m_oPosList[i], self.m_oPosList[i + 1])
+			
+			# 직선 일 경우
+			elif self.m_eSelTool == CPractice_10.EP10Tool.LINE:
+				oPainter.drawLine(self.m_oPosList[0], self.m_oPosList[nNumPositions - 1])
+			
+			# 타원 일 경우
+			elif self.m_eSelTool == CPractice_10.EP10Tool.ELLIPSE:
+				oPainter.drawEllipse(QRect(self.m_oPosList[0], self.m_oPosList[nNumPositions - 1]))
+			
+			# 사각형 일 경우
+			elif self.m_eSelTool == CPractice_10.EP10Tool.RECTANGLE:
+				oPainter.drawRect(QRect(self.m_oPosList[0], self.m_oPosList[nNumPositions - 1]))
+				
 		finally:
 			oPainter.end()
 	
@@ -92,15 +113,16 @@ class CPractice_10(QMainWindow, uic.loadUiType("Resources/Practice_10/P10MainWin
 			
 	# 마우스 이동 이벤트를 수신했을 경우
 	def mouseMoveEvent(self, a_oEvent: QMouseEvent):
-		pass
+		self.m_oPosList.append(QPoint(a_oEvent.x(), a_oEvent.y()))
 	
 	# 마우스 버튼 눌림 이벤트를 수신했을 경우
 	def mousePressEvent(self, a_oEvent: QMouseEvent):
-		pass
+		self.m_oPosList.clear()
+		self.m_oPosList.append(QPoint(a_oEvent.x(), a_oEvent.y()))
 	
 	# 마우스 버튼 눌림 종료 이벤트를 수신했을 경우
 	def mouseReleaseEvent(self, a_oEvent: QMouseEvent):
-		pass
+		self.mouseMoveEvent(a_oEvent)
 	
 	# 정보 메뉴를 눌렀을 경우
 	def OnClickAboutMenu(self):
@@ -109,6 +131,7 @@ class CPractice_10(QMainWindow, uic.loadUiType("Resources/Practice_10/P10MainWin
 	# 툴 버튼을 눌렀을 경우
 	def OnClickToolBtn(self, a_eTool: EP10Tool):
 		self.m_eSelTool = a_eTool
+		self.m_oPosList.clear()
 	
 	# 초기화
 	@classmethod
