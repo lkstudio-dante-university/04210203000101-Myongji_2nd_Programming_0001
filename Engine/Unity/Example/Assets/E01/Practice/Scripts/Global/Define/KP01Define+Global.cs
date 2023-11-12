bf4d18a07e63224bef523c2bc1d1967fca8cf4ce
dante <lkstudio.dante@gmail.com>
2023-11-12 15:48:43 +0900

    no message

---
+++ Engine/Unity/Example/Assets/E01/Practice/Scripts/Global/Define/KP01Define+Global.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace P01 {
+	/** 전역 상수 */
+	public static partial class KP01Define {
+		#region 기본
+		// 씬 이름
+		public const string G_SCENE_N_PRACTICE_02 = "P01Practice_02";
+		#endregion // 기본
+	}
+}
